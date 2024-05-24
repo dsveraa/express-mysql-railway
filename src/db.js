927c2a25e790ddf151b0e509918da85f924cee5e
David Vera <dsvera.a@gmail.com>
2024-05-23 22:51:12 -0400

    quité las variables de entorno

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 import { createPool } from "mysql2/promise"
-import { MYSQLHOST, MYSQLUSER, MYSQLPASSWORD, MYSQL_DATABASE, MYSQLPORT} from './config.js'
+// import { MYSQLHOST, MYSQLUSER, MYSQLPASSWORD, MYSQL_DATABASE, MYSQLPORT} from './config.js'
 
 export const pool = createPool({
-    user: MYSQLUSER,
-    password: MYSQLPASSWORD,
-    host: MYSQLHOST,
-    port: MYSQLPORT,
-    database: MYSQL_DATABASE
+    host: 'viaduct.proxy.rlwy.net',
+    port: 56936,
+    user: 'root',
+    password: 'zsuBaaeJMjYFdAMjNIgsfuDJwuTgGwgY',
+    database: 'railway'
 })
