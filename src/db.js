18893a1fd1bd9eae64405ad25aa98b5b7aac4ca0
David Vera <dsvera.a@gmail.com>
2024-05-24 16:13:44 -0400

    nuevas variables de entorno

--- src/db.js
+++ src/db.js
@@ -2,9 +2,9 @@ import { createPool } from "mysql2/promise"
 import { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT} from './config.js'
 
 export const pool = createPool({
-    host: DB_HOST,
-    port: DB_PORT,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_DATABASE
+    host: DB_HOST || 'localhost',
+    port: DB_PORT || 3306,
+    user: DB_USER || 'root',
+    password: DB_PASSWORD || 'root',
+    database: DB_DATABASE || 'usersdb'
 })
